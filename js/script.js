6043b9872156aebdae29016809494e37b86493e4
sugarrobert <robert.sugar@hotmail.com>
2021-01-18 22:23:16 +0100

    added functions and event listeners for scrolling in cart when its more then 3 items

--- js/script.js
+++ js/script.js
@@ -1,8 +1,59 @@
-const cartButton = document.getElementById('shopping-cart-button');
-const shoppingCartList = document.getElementById('shopping-cart-list');
+(function () {
+  const cartButton = document.getElementById('shopping-cart-button');
+  const shoppingCartList = document.getElementById('shopping-cart-list');
+  const scrollUp = document.getElementById('scroll-arrow-up');
+  const scrollDown = document.getElementById('scroll-arrow-down');
+  const items = document.querySelectorAll('.shopping-cart-item');
 
-cartButton.addEventListener('click', (e) => {
-  e.preventDefault();
+  cartButton.addEventListener('click', (e) => {
+    e.preventDefault();
 
-  shoppingCartList.classList.toggle('show-cart');
-});
+    shoppingCartList.classList.toggle('show');
+  });
+
+  let start = 1;
+  let end = 4;
+  const itemsArr = Array.from(items);
+
+  function showItemsInCart() {
+    const arr = itemsArr.slice(start, end);
+
+    arr.forEach((item) => {
+      item.classList.add('show');
+      item.classList.remove('hide');
+    });
+  }
+
+  function hideItemsInCart() {
+    itemsArr.forEach((item) => {
+      item.classList.add('hide');
+      item.classList.remove('show');
+    });
+  }
+
+  showItemsInCart();
+
+  scrollUp.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (start === 0) {
+      // scrollUp.style.pointerEvents = 'none';
+    } else {
+      start--;
+      end--;
+      hideItemsInCart();
+      showItemsInCart(start, end);
+    }
+  });
+
+  scrollDown.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (end === itemsArr.length) {
+      // scrollUp.style.pointerEvents = 'none';
+    } else {
+      start++;
+      end++;
+      hideItemsInCart();
+      showItemsInCart(start, end);
+    }
+  });
+})();
